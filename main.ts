5f2b48494cdd6ffea7f5ea306c4b68d6355db22e
mark-lutabi <mark.lutabi@mths.ca>
2024-11-01 10:54:03 -0400

    code

--- main.ts
+++ main.ts
@@ -1,8 +1,29 @@
+
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
+ * Created by: Mark Lutabi
+ * Created on: October, 2024
  * This program ...
 */
 
-basic.showString('Hello, World!')
+// setup
+basic.showIcon(IconNames.Happy)
+
+let distanceOfObject: number
+
+// loop forever
+while (true) {
+    distanceOfObject = sonar.ping(
+        DigitalPin.P1,
+        DigitalPin.P2,
+        PingUnit.Centimeters
+    )
+
+    if (distanceOfObject < 10) {
+        robotbit.StpCarMove(0, 0)
+        basic.showIcon(IconNames.No)
+    } else {
+        robotbit.StpCarMove(10, 48)
+        basic.showIcon(IconNames.Yes)
+    }
+}
